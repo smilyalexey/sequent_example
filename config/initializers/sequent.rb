109789c14d0b3ed955e2e242d3503a6d0631823c
Alexey Volobuev <alexey@smily.com>
2023-07-10 13:09:14 +0300

    First commit

---
+++ config/initializers/sequent.rb
@@ -0,0 +1,20 @@
+require_relative '../../db/sequent_migrations'
+   
+Rails.application.reloader.to_prepare do
+  Sequent.configure do |config|
+    config.migrations_class_name = 'SequentMigrations'
+   
+    config.command_handlers = [
+      # add you Sequent::CommandHandler's here
+    ]
+   
+    config.event_handlers = [
+      # add you Sequent::Projector's or Sequent::Workflow's here
+    ]
+
+    config.database_config_directory = 'config'
+     
+    # this is the location of your sql files for your view_schema
+    config.migration_sql_files_directory = 'db/sequent'
+  end
+end
